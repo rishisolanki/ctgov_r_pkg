7288e7090c7a2a36c89f7bb0407725424288d9b1
rishi solanki <rsolanki@seas.upenn.edu>
2016-07-06 14:46:49 -0400

    Search for specific clinical trials

---
+++ R/search_clinical_trial.R
@@ -0,0 +1,17 @@
+#' Get clinical trial data by matching specific NCT_ID for the specific kterm
+#' @param kterm A string representing a disease name or primary condition
+#' @output list
+#' @export
+
+search_clinical_trial <- function(kterm){
+  # get nct_id for the specific disease
+  match_nct_id <- data.frame(get_matching_kterm_nct_id(kterm))
+
+  data0 <- read_processed_clinical_data()
+  colnames(match_nct_id) <- "NCT_ID"
+
+  # get specific clinical trial data
+  output <- merge(match_nct_id,data0,by.x = "NCT_ID", by.y = "NCT_ID")
+
+  return(output)
+}
