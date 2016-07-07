6d3a2d1ede50f2ab5bb964794125361c4924934e
rishi solanki <rsolanki@seas.upenn.edu>
2016-07-07 11:29:49 -0400

    Added input parameter validation

--- R/plot_k_values.R
+++ R/plot_k_values.R
@@ -9,6 +9,18 @@
 #' @export
 #'
 plot_k_values <- function(get_k_value_op, start_date, end_date, kterm = "", location = ""){
+
+  if(get_k_value_op == ""){
+    stop("Please enter a correct input parameters")
+  }
+
+  if(start_date == "" | end_date == ""){
+    stop("Please check start and end date")
+  }
+
+  if(is.na(as.yearmon(start_date)) | is.na(as.yearmon(end_date))){
+    stop ("Please enter dates in MM YYYY format i.e Jan 2012")
+  }
   p1 <- get_k_value_op
   p <- append(0,p1) # append zero because step function reduces length by 1
   sfunP <- stepfun(1:(length(p)-1),p,f=0)
